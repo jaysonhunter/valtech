7b39c229319b3a03ccfaa5b3dac8f5ef133bdcb0
Jayson Hunter <jhunter@jaysons-imac.home>
2017-12-09 13:00:41 +0000

    Initial empty test.js file

---
+++ src/js/test.js
@@ -0,0 +1,28 @@
+/**
+    * Valtech UI exercise
+    * Author: Jayson Hunter
+    * Date: December 2017
+    * Version: 1.0.0
+*/
+
+
+var valtech = (function(){
+
+
+    var init = function() {
+
+
+    }
+
+
+
+    return {
+        init: init
+    };
+
+})();
+
+
+$(document).ready(function() {
+    valtech.init();
+})
